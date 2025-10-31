0fb9b82dc073810ff9244a95c96d60a4a50d14b5
eclipse toure <code@blackbox.ai>
2025-10-31 19:47:38 +0100

    add alias plugin

--- build.ts
+++ build.ts
@@ -24,7 +24,7 @@ await build({
   minify: true,
   sourcemap: true,
   sourcesContent: true,
-  bundle: false,
+  bundle: true,
   metafile: true,
   plugins: [aliasSrcPlugin()],
 }).catch((error) => {

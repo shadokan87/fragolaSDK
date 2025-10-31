a49b3c465980e4946a7b35f88643946ccbad0165
shadokan87 <shinobi87@live.fr>
2025-10-31 19:47:38 +0100

    add alias plugin

--- esbuild.aliasSrcPlugin.js
+++ esbuild.aliasSrcPlugin.js
@@ -3,14 +3,19 @@ import path from 'path';
 /**
  * Simple esbuild plugin to resolve @src/* to ./src/*
  */
+import fs from 'fs';
 export function aliasSrcPlugin() {
   return {
     name: 'alias-src',
     setup(build) {
       build.onResolve({ filter: /^@src\// }, args => {
-        // Remove @src/ and resolve to ./src/...
-        const relPath = args.path.replace(/^@src\//, './src/');
-        return { path: path.resolve(process.cwd(), relPath) };
+        let relPath = args.path.replace(/^@src\//, './src/');
+        let absPath = path.resolve(process.cwd(), relPath);
+        // If no extension and file doesn't exist, try .ts
+        if (!path.extname(absPath) && !fs.existsSync(absPath) && fs.existsSync(absPath + '.ts')) {
+          absPath += '.ts';
+        }
+        return { path: absPath };
       });
     },
   };

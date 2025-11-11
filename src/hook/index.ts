f3746c93202644df1c4983848e36b0febb0db512
Eclipse TOURE <motoure@f6r13s8.clusters.42paris.fr>
2025-11-11 20:20:22 +0100

    🚧 Wip mcp client

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,5 +1,8 @@
+import type { maybePromise } from "@src/types";
 import {Agent} from "../agent";
 
 export type FragolaHook = (agent: Agent) => void;
+
+export type FragolaHookDispose = () => maybePromise<void>;
 
 export const Hook = (callback: FragolaHook) => callback;

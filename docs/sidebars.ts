be03b7bd1bd3584b5c7e90039731480fa503dacd
shadokan87 <shinobi87@live.fr>
2025-11-13 17:12:59 +0100

    🚧 Wip docs

---
+++ docs/sidebars.ts
@@ -0,0 +1,10 @@
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  docs: [
+    'overview',
+    'get-started'
+  ],
+};
+
+export default sidebars;

4e91e88f224b20d8b2d699f3a8762ff81a20f157
Eclipse TOURE <motoure@f6r8s11.clusters.42paris.fr>
2025-11-15 15:54:05 +0100

    ♻️ Now using verdaccio for package testing

--- package/hook.index.ts
+++ package/hook.index.ts
@@ -1,12 +1,1 @@
-import { Hook } from "@src/hook/index";
-// Types
-import type { FragolaHook, FragolaHookDispose } from "@src/hook/index";
-
-export {
-    Hook,
-}
-
-export type {
-    FragolaHook,
-    FragolaHookDispose
-}
+export * from "@src/hook/index";

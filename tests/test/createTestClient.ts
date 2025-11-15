2e49d8575425d47e1aea7734cea6ba86555c6e1b
Eclipse TOURE <motoure@f6r8s11.clusters.42paris.fr>
2025-11-15 15:54:05 +0100

    ♻️ Now using verdaccio for package testing

--- tests/test/createTestClient.ts
+++ tests/test/createTestClient.ts
@@ -1,14 +1,16 @@
 import { createHeaders, PORTKEY_GATEWAY_URL } from 'portkey-ai';
 import { Fragola, type ClientOptions } from "@fragola-ai/agentic-sdk-core";
-
+import 'dotenv/config';
 export const createTestClient = (opts?: ClientOptions) => {
     const defaultOpts: ClientOptions = {
         baseURL: PORTKEY_GATEWAY_URL,
+        apiKey: "xxx",
         defaultHeaders: createHeaders({
             virtualKey: "google-966377",
             apiKey: process.env["TEST_API_KEY"]
         }),
         model: process.env["TEST_MODEL_MEDIUM"]!
     }
-    return new Fragola(opts ? {...opts, ...defaultOpts} : defaultOpts);
+    const _opts = opts ? {...opts, ...defaultOpts} : defaultOpts;
+    return new Fragola(_opts);
 }

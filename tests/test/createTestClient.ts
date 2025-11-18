f5827a2816d4649ecb1d43a6f9614b6fd314a749
shadokan87 <shinobi87@live.fr>
2025-11-18 20:10:59 +0100

    ✅ Added tests for streaming

--- tests/test/createTestClient.ts
+++ tests/test/createTestClient.ts
@@ -2,15 +2,16 @@ import { createHeaders, PORTKEY_GATEWAY_URL } from 'portkey-ai';
 import { Fragola, type ClientOptions } from "@fragola-ai/agentic-sdk-core";
 import 'dotenv/config';
 export const createTestClient = (opts?: ClientOptions) => {
-    const defaultOpts: ClientOptions = {
-        baseURL: PORTKEY_GATEWAY_URL,
-        apiKey: "xxx",
-        defaultHeaders: createHeaders({
-            virtualKey: "google-966377",
-            apiKey: process.env["TEST_API_KEY"]
-        }),
-        model: process.env["TEST_MODEL_MEDIUM"]!
-    }
-    const _opts = opts ? {...opts, ...defaultOpts} : defaultOpts;
-    return new Fragola(_opts);
+  const defaultOpts: ClientOptions = {
+    baseURL: PORTKEY_GATEWAY_URL,
+    apiKey: "xxx",
+    defaultHeaders: createHeaders({
+      virtualKey: "google-966377",
+      apiKey: process.env["TEST_API_KEY"],
+      Authorization: `Bearer ${process.env["TEST_GCLOUD_AUTH_TOKEN"]}`
+    }),
+    model: process.env["TEST_MODEL_MEDIUM"]!
+  }
+  const _opts = opts ? { ...opts, ...defaultOpts } : defaultOpts;
+  return new Fragola(_opts);
 }

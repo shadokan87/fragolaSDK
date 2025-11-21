bf3d703e4cea9569d94aae258e1dd7ed0c6dba0e
shadokan87 <shinobi87@live.fr>
2025-11-21 23:00:39 +0100

    💄 Changed accent color in docs

--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -1,9 +1,15 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: (
+        <>
+          <Image src="/logo.png" alt="Fragola" width={54} height={54} />
+          <span>Fragola SDK</span>
+        </>
+      ),
     },
   };
 }

9b9ef57f6079c14a323a0d7f0925e077540815c9
shadokan87 <shinobi87@live.fr>
2025-11-30 13:46:10 +0100

    rewrites path next

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -7,6 +7,12 @@ const config = {
   reactStrictMode: true,
   async rewrites() {
     return [
+      // Proxy Next.js static assets to the external site
+      {
+        source: '/_next/:path*',
+        destination: 'https://fragola-sdk-landing.vercel.app/_next/:path*',
+      },
+      // Proxy all other requests to the external site
       {
         source: '/:path*',
         destination: 'https://fragola-sdk-landing.vercel.app/:path*',

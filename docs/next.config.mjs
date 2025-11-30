d33157ad4dcf4b9763f03ccafc8e710723f31cfb
shadokan87 <shinobi87@live.fr>
2025-11-30 14:34:48 +0100

    assets prefix

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  assetPrefix: "https://www.fragola.ai",
   async rewrites() {
     return {
       fallback: [

25e78f65ef49464282779ea14d145d9e82693554
shadokan87 <shinobi87@live.fr>
2025-11-30 14:52:48 +0100

    updated next config

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -6,20 +6,10 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: '/_next/:path*',
-          destination: 'https://fragola-sdk-landing.vercel.app/_next/:path*',
-        },
-      ],
-      fallback: [
-        {
-          source: '/:path*',
-          destination: 'https://fragola-sdk-landing.vercel.app/:path*',
-        },
-      ],
-    };
+    return [{
+      source: "/",
+      destination: "https://fragola-sdk-landing.vercel.app"
+    }]
   },
 };
 

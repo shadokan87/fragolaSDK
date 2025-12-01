f74b275743ecfdf980d84b56e8f2b3226a3971c9
shadokan87 <shinobi87@live.fr>
2025-12-01 13:26:59 +0100

    rewrite to landing page

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://fragola-sdk-landing.vercel.app/:path*"
+      }
+    ]
+  }
 };
 
 export default withMDX(config);

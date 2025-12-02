bc58f08432241bf06ce7de73893edd3723419606
shadokan87 <shinobi87@live.fr>
2025-12-02 11:08:32 +0100

    moved docs at root instead of /docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,9 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  assetPrefix: '/docs',
-  cleanUrls: true,
-  trailingSlash: true,
 };
 
 export default withMDX(config);

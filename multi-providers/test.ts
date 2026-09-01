7256e6447c079726e6a2fcb28afeb6d41f41cf4a
shadokan87 <shinobi87@live.fr>
2026-09-02 01:14:34 +0200

    wip need to insert credentials

--- multi-providers/test.ts
+++ multi-providers/test.ts
@@ -4,13 +4,14 @@ import { createLocalGatewayFetch, provider } from "./src";
 const fragola = new Fragola({
     model: "@google/gemini-3.7-flash",
     apiKey: process.env["GOOGLE_API_KEY"],
+    baseURL: "http://127.0.0.1/v1",
     fetch: createLocalGatewayFetch()
 });
 
 const agent = fragola.agent({
     name: "assistant",
     description: "",
-    instructions: ""
+    instructions: "you are a helpful assistant"
 })
 // .use(provider("google", {}));
 // await agent.init();
